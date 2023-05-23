3ba4524bc6aaa75a25a211d43962e1788596461b
tfumi <40786633+fumiaki-todaka@users.noreply.github.com>
2023-05-24 01:27:11 +0900

    修正

--- server.js
+++ server.js
@@ -44,7 +44,7 @@ cors_proxy.createServer({
     // Do not add X-Forwarded-For, etc. headers, because Heroku already adds it.
     xfwd: false,
   },
-  corsMaxAge: 999999999999999999,
+  corsMaxAge: 86400,
 }).listen(port, host, function() {
   console.log('Running CORS Anywhere on ' + host + ':' + port);
 });
