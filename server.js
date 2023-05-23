68fab39cc49de39a2700a958ea0671b72ec5f448
tfumi <40786633+fumiaki-todaka@users.noreply.github.com>
2023-05-24 01:39:21 +0900

    Revert "修正"
    This reverts commit 3ba4524bc6aaa75a25a211d43962e1788596461b.

--- server.js
+++ server.js
@@ -44,7 +44,7 @@ cors_proxy.createServer({
     // Do not add X-Forwarded-For, etc. headers, because Heroku already adds it.
     xfwd: false,
   },
-  corsMaxAge: 86400,
+  corsMaxAge: 999999999999999999,
 }).listen(port, host, function() {
   console.log('Running CORS Anywhere on ' + host + ':' + port);
 });
